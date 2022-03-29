753d462926929f9967e0bfbe6d377ad9f0eacdb8
Tam Le <tamhle824@gmail.com>
2022-03-29 12:16:11 -0500

    strings

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args){
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        System.out.println(myTrue);
+        System.out.println(myFalse);
+      
+    
+        
+
+
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # java_masterclass
+
+primitive types
+    -byte -128 - +128
+    -short rarely use
+    -int 
+    -long
+    -float rarely use
+    -double
+    -char
+    -boolean
+
+
+Class 
+    -string: sequence of characters. 
